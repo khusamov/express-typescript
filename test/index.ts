762a40b136237920cc5382f1dad6f9c9e5f8f2ee
Khusamov Sukhrob <khusamov@yandex.ru>
2017-03-23 08:32:08 +0000

    Подправлена инструкция ридми

--- test/index.ts
+++ test/index.ts
@@ -4,9 +4,9 @@ import { Application } from '../index';
 let app = new Application();
 
 app.get('/', function (req, res) {
-    res.send('Hello World!')
+	res.send('Hello World!')
 })
 
 app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+	console.log('Example app listening on port 8080!')
 })
